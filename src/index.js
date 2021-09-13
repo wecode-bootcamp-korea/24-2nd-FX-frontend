ffbcd0a30f5816ccd93bbd13a2c47ee29e87b52d
김승찬 <kimseungchan@gimseungchan-ui-MacBookAir.local>
2021-09-13 19:30:08 +0900

    Add : 초기세팅 수정 (index.js 파일 수정 및 GlobalStyles 수정)

---
+++ src/index.js
@@ -0,0 +1,16 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import GlobalStyles from "./styles/GlobalStyles";
+import { ThemeProvider } from "styled-components";
+import Routes from "./Routes";
+import theme from "./styles/theme";
+
+ReactDOM.render(
+  <>
+    <GlobalStyles />
+    <ThemeProvider theme={theme}>
+      <Routes />
+    </ThemeProvider>
+  </>,
+  document.getElementById("root")
+);
