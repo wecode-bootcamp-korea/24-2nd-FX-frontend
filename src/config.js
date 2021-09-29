b5cfbdff8969395ede279b58c7294c03e398925f
eunjeong-97 <beanlove97@gmail.com>
2021-09-29 17:14:52 +0900

    WIP: 위시리스트페이지 테스트중

--- src/config.js
+++ src/config.js
@@ -4,12 +4,13 @@ export const DETAIL_URL = `http://${MAIN_IP}:8000/content/`;
 export const WISH_URL = `http://${MAIN_IP}:8000/wishlists`;
 export const STREAM_URL = `http://${MAIN_IP}:8000/content/streaming/`;
 
-const LOGIN_IP = "10.58.6.10";
-export const FLIX_SIGNIN_URL = `http://${LOGIN_IP}:8000/users/sign-in`;
-export const FLIX_SIGNUP_URL = `http://${LOGIN_IP}:8000/users/sign-in`;
-export const KAKAO_URL = `http://${LOGIN_IP}:8000/users/kakao-login`;
-export const GOOGLE_URL = `http://${LOGIN_IP}:8000/users/google-login`;
-export const WISH_TOKEN =
+const LOGIN_URL = "http://10.58.1.98:8000/users";
+export const FLIX_SIGNIN_URL = `${LOGIN_URL}/sign-in`;
+export const FLIX_SIGNUP_URL = `${LOGIN_URL}/sign-up`;
+export const KAKAO_URL = `${LOGIN_URL}/kakao-login`;
+export const GOOGLE_URL = `${LOGIN_URL}/google-login`;
+
+export const FLIX_TOKEN =
   "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ1c2VyX2lkIjoxLCJleHAiOjE5MzIzMTExNDJ9.b1opm3drEjYmZVTHbE_EVVcWPCzTsjKexRmOCN4bLsM";
 export const GOOGLE_CLIENT_ID =
   "134318900672-6uplcu4p89po232gdj8vq2i660hs0d7p.apps.googleusercontent.com";
